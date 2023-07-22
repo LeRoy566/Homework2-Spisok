8e55fbf236b2c54d049ef260acc0de064f267e9c
Leonid <Lyunonin@yandex.ru>
2023-07-22 19:35:17 +0300

    50%

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
+function randomNumberInRange(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+const generateNumbers = () => {
+  const newMass = [1,2,3,4,5];
+  shuffle(newMass)
+  return newMass;
+};
 function App() {
+  const [numbers, setNumbers] = useState(generateNumbers());
+  const [maxNumber, setMaxNumber] = useState(5);
+  const remove = (item, index) => {
+    if (item == maxNumber) {
+     const newMass = numbers.slice()
+     newMass.splice(index, 1)
+     setMaxNumber(maxNumber-1)
+     if (newMass.length == 0) {
+      alert("Win")
+     } else{
+      setNumbers(newMass)
+     }
+    } else{
+      alert("Loose")
+    }
+  }
+    
+    
+  
+  const Nie = numbers.map((item, index) => {
+    return <li key={index} onClick={() => remove(item, index)}>{item}</li>;
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>{Nie}</ul>
     </div>
   );
 }
